a9f876aa041828bae031c957ae62298774c1605f
filmee24 <filmee24@gmail.com>
2022-03-07 17:34:45 +0100

    Cleanup

--- Source/TestProject1/UnitTest1.cs
+++ Source/TestProject1/UnitTest1.cs
@@ -14,14 +14,25 @@ namespace TestProject1
             var src = "declare hello : i32 = 42;";
             var ast = CompilationUnit.FromText(src);
 
-            var statement = ast.Body.Body.OfType<VariableDeclarationStatement>().FirstOrDefault();
+            var statement = ParseAndGetNode<VariableDeclarationStatement>(src);
 
-            Assert.IsNotNull(statement);
             Assert.AreEqual(statement.NameToken.Text, "hello");
             Assert.AreEqual(statement.TypeToken.Text, "i32");
 
             Assert.IsInstanceOfType(statement.Value, typeof(LiteralNode));
             Assert.AreEqual(((LiteralNode)statement.Value).Value, 42);
+        }
+
+        private static T ParseAndGetNode<T>(string source)
+        {
+            var src = "declare hello : i32 = 42;";
+            var ast = CompilationUnit.FromText(src);
+
+            var node = ast.Body.Body.OfType<T>().FirstOrDefault();
+
+            Assert.IsNotNull(node);
+
+            return node;
         }
     }
 }
