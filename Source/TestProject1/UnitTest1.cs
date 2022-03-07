fc5debeca58c8ab4d88f0b8341ec57c0fe339b83
filmee24 <filmee24@gmail.com>
2022-03-07 17:32:16 +0100

    Cleanup

--- Source/TestProject1/UnitTest1.cs
+++ Source/TestProject1/UnitTest1.cs
@@ -1,4 +1,3 @@
-using Backlang_Compiler.Parsing;
 using Backlang_Compiler.Parsing.AST;
 using Backlang_Compiler.Parsing.AST.Statements;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,18 +9,7 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void Lexer_Multiple_Char_Symbol_Should_Pass()
-        {
-            var src = "a <-> b";
-            var lexer = new Lexer();
-            var tokens = lexer.Tokenize(src);
-
-            Assert.AreEqual(4, tokens.Count);
-            Assert.AreEqual(tokens[1].Type, TokenType.SwapOperator);
-        }
-
-        [TestMethod]
-        public void VariableDeclarationFull_Should_Pass()
+        public void VariableDeclaration_Full_Should_Pass()
         {
             var src = "declare hello : i32 = 42;";
             var ast = CompilationUnit.FromText(src);
