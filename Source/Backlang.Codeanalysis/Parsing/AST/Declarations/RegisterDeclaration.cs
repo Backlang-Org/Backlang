76f506c19608c4f5626612b4227f01e9b514d411
filmee24 <filmee24@gmail.com>
2022-03-15 19:35:24 +0100

    Add RegisterDeclaration

---
+++ Source/Backlang.Codeanalysis/Parsing/AST/Declarations/RegisterDeclaration.cs
@@ -0,0 +1,29 @@
+﻿namespace Backlang.Codeanalysis.Parsing.AST.Declarations;
+
+public class RegisterDeclaration : SyntaxNode, IParsePoint<SyntaxNode>
+{
+    public Expression Address { get; set; }
+    public string Name { get; set; }
+
+    public static SyntaxNode Parse(TokenIterator iterator, Parser parser)
+    {
+        var node = new RegisterDeclaration();
+
+        node.Name = iterator.Current.Text;
+
+        iterator.NextToken();
+
+        iterator.Match(TokenType.EqualsToken);
+
+        node.Address = Expression.Parse(parser);
+
+        iterator.Match(TokenType.Semicolon);
+
+        return node;
+    }
+
+    public override T Accept<T>(IVisitor<T> visitor)
+    {
+        return visitor.Visit(this);
+    }
+}
---
+++ Source/TestProject1/RegisterTests.cs
@@ -0,0 +1,43 @@
+﻿using Backlang.Codeanalysis.Parsing.AST;
+using Backlang.Codeanalysis.Parsing.AST.Declarations;
+using Backlang.Codeanalysis.Parsing.AST.Expressions;
+using Backlang.Codeanalysis.Parsing.AST.Statements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1;
+
+[TestClass]
+public class RegisterTests : ParserTestBase
+{
+    [TestMethod]
+    public void DeclareRegister_Should_Pass()
+    {
+        var src = "register EAX = 1;";
+        var tree = ParseAndGetNode<RegisterDeclaration>(src);
+
+        Assert.AreEqual(tree.Name, "EAX");
+        Assert.AreEqual(((LiteralNode)tree.Address).Value, 1L);
+    }
+
+    [TestMethod]
+    public void UseRegister_Should_Pass()
+    {
+        var src = "#eax;";
+        var tree = ParseAndGetNodeInFunction<ExpressionStatement>(src);
+        var expression = (UnaryExpression)tree.Expression;
+
+        Assert.AreEqual(expression.OperatorToken.Text, "#");
+        Assert.AreEqual(((NameExpression)expression.Expression).Name, "eax");
+    }
+
+    [TestMethod]
+    public void UseRegister_With_Assignment_Should_Pass()
+    {
+        var src = "something = #eax;";
+        var tree = ParseAndGetNodeInFunction<ExpressionStatement>(src);
+        var expression = (BinaryExpression)tree.Expression;
+
+        Assert.AreEqual(expression.OperatorToken.Text, "=");
+        Assert.AreEqual(((UnaryExpression)expression.Right).OperatorToken.Text, "#");
+    }
+}
