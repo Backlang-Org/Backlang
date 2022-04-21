39b311c6887734339028a712cd4227f8540d453e
filmee24 <filmee24@gmail.com>
2022-04-21 23:06:59 +0200

    Cleanup

--- Source/Backlang-Compiler/Program.cs
+++ Source/Backlang-Compiler/Program.cs
@@ -1,4 +1,4 @@
-﻿using Backlang_Compiler.Compiling.Stages;
+﻿using Backlang.Driver;
 using CommandLine;
 
 namespace Backlang_Compiler;
@@ -7,24 +7,9 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var pipeline = Flo.Pipeline.Build<CompilerContext, CompilerContext>(
-       cfg => {
-           cfg.Add<ParsingStage>();
-           cfg.Add<InitTypeSystemStage>();
-
-           cfg.When(_ => _.References.Any(), _ => {
-               _.Add<InitReferencesStage>();
-           });
-
-           cfg.Add<LowererStage>();
-           cfg.Add<IntermediateStage>();
-           cfg.Add<ImplementationStage>();
-       }
-   );
-
         Parser.Default.ParseArguments<CompilerContext>(args)
-              .WithParsed(async o => {
-                  _ = await pipeline.Invoke(o);
+              .WithParsed(context => {
+                  CompilerDriver.Compile(context);
               });
     }
 }
