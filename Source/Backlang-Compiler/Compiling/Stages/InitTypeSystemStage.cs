fbee5dbbe26acec796a25c0ac7e063102475bd6a
filmee24 <filmee24@gmail.com>
2022-04-21 19:26:03 +0200

    Add Collections.Spezialized To TypeModel

--- Source/Backlang-Compiler/Compiling/Stages/InitTypeSystemStage.cs
+++ Source/Backlang-Compiler/Compiling/Stages/InitTypeSystemStage.cs
@@ -2,6 +2,7 @@
 using Flo;
 using Furesoft.Core.CodeDom.Compiler.Core.Names;
 using Furesoft.Core.CodeDom.Compiler.Core.TypeSystem;
+using System.Collections.Specialized;
 
 namespace Backlang_Compiler.Compiling.Stages;
 
@@ -13,12 +14,15 @@ public sealed class InitTypeSystemStage : IHandler<CompilerContext, CompilerCont
 
         var corLib = ClrTypeEnvironmentBuilder.CollectTypes(typeof(uint).Assembly);
         var consoleLib = ClrTypeEnvironmentBuilder.CollectTypes(typeof(Console).Assembly);
+        var collectionsLib = ClrTypeEnvironmentBuilder.CollectTypes(typeof(BitVector32).Assembly);
 
         context.Binder.AddAssembly(corLib);
         context.Binder.AddAssembly(consoleLib);
+        context.Binder.AddAssembly(collectionsLib);
 
         ClrTypeEnvironmentBuilder.FillTypes(typeof(uint).Assembly, context.Binder);
         ClrTypeEnvironmentBuilder.FillTypes(typeof(Console).Assembly, context.Binder);
+        ClrTypeEnvironmentBuilder.FillTypes(typeof(BitVector32).Assembly, context.Binder);
 
         context.Environment = new Furesoft.Core.CodeDom.Backends.CLR.CorlibTypeEnvironment(corLib);
 
