534cbe1e46a9ee04be74517048dc0522f6192f97
filmee24 <filmee24@gmail.com>
2022-04-23 23:53:12 +0200

    Add Expand Function ReturnType SELF

--- Source/Backlang.Codeanalysis/Parsing/AST/Declarations/ImplementationDeclaration.cs
+++ Source/Backlang.Codeanalysis/Parsing/AST/Declarations/ImplementationDeclaration.cs
@@ -6,63 +6,54 @@ public class ImplementationDeclaration : IParsePoint<LNode>
 {
     public static LNode Parse(TokenIterator iterator, Parser parser)
     {
-        if (iterator.Current.Type == TokenType.For || iterator.Current.Type == TokenType.Of)
+        iterator.NextToken();
+
+        bool isStatic = false;
+        if (iterator.Current.Type == TokenType.Static)
         {
+            isStatic = true;
             iterator.NextToken();
+        }
 
-            bool isStatic = false;
-            if (iterator.Current.Type == TokenType.Static)
+        LNode target = null;
+        var targets = new LNodeList();
+
+        while (iterator.Current.Type != TokenType.OpenCurly)
+        {
+            if (iterator.Peek(1).Type == TokenType.RangeOperator)
             {
-                isStatic = true;
-                iterator.NextToken();
-            }
-
-            LNode target = null;
-            var targets = new LNodeList();
-
-            while (iterator.Current.Type != TokenType.OpenCurly)
-            {
-                if (iterator.Peek(1).Type == TokenType.RangeOperator)
-                {
-                    targets.Add(Expression.Parse(parser));
-                }
-                else
-                {
-                    targets.Add(TypeLiteral.Parse(iterator, parser));
-                }
-
-                if (iterator.Current.Type != TokenType.OpenCurly)
-                {
-                    iterator.Match(TokenType.Comma);
-                }
-            }
-
-            if (targets.Count == 1)
-            {
-                target = targets[0];
+                targets.Add(Expression.Parse(parser));
             }
             else
             {
-                target = LNode.Call(Symbols.ToExpand, targets);
+                targets.Add(TypeLiteral.Parse(iterator, parser));
             }
 
-            iterator.Match(TokenType.OpenCurly);
-
-            LNodeList body = new();
-            while (iterator.Current.Type != TokenType.EOF && iterator.Current.Type != TokenType.CloseCurly)
+            if (iterator.Current.Type != TokenType.OpenCurly)
             {
-                body.Add(parser.InvokeParsePoint(parser.DeclarationParsePoints));
+                iterator.Match(TokenType.Comma);
             }
-
-            iterator.Match(TokenType.CloseCurly);
-
-            return SyntaxTree.ImplDecl(target, body, isStatic);
         }
 
-        parser.Messages.Add(
-            Message.Error(parser.Document,
-                "Expected For Or Of", iterator.Current.Line, iterator.Current.Column));
+        if (targets.Count == 1)
+        {
+            target = targets[0];
+        }
+        else
+        {
+            target = LNode.Call(Symbols.ToExpand, targets);
+        }
 
-        return LNode.Missing;
+        iterator.Match(TokenType.OpenCurly);
+
+        LNodeList body = new();
+        while (iterator.Current.Type != TokenType.EOF && iterator.Current.Type != TokenType.CloseCurly)
+        {
+            body.Add(parser.InvokeParsePoint(parser.DeclarationParsePoints));
+        }
+
+        iterator.Match(TokenType.CloseCurly);
+
+        return SyntaxTree.ImplDecl(target, body, isStatic);
     }
 }
