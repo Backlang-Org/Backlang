6656ee1801befb3d3e5bc725c97dab8b329d1910
filmee24 <filmee24@gmail.com>
2022-05-25 09:24:16 +0200

    Cleanup

--- Source/Backlang.Core/Macros/BuiltInMacros.cs
+++ Source/Backlang.Core/Macros/BuiltInMacros.cs
@@ -11,7 +11,7 @@ public static partial class BuiltInMacros
     private static LNodeFactory F = new LNodeFactory(EmptySourceFile.Synthetic);
 
     [LexicalMacro(@"nameof(id_or_expr)",
-        @"Converts the 'key' name component of an expression to a string (e.g. nameof(A.B<C>(D)) == ""B"")", Mode = MacroMode.MatchIdentifierOrCall)]
+        @"Converts the 'key' name component of an expression to a string (e.g. nameof(A.B<C>(D)) == ""B"")", "nameof", Mode = MacroMode.MatchIdentifierOrCall)]
     public static LNode @Nameof(LNode nameof, IMacroContext context)
     {
         if (nameof.ArgCount != 1)
@@ -47,8 +47,8 @@ public static partial class BuiltInMacros
         return arg;
     }
 
-    [LexicalMacro("concatId(id, id)", "concatId", "Concats 2 Ids to a new Id (eg. concatId(a, b) == ab)", Mode = MacroMode.MatchIdentifierOrCall)]
-    public static LNode ConcatId(LNode concatID, IMacroContext context)
+    [LexicalMacro("concatId(id, id)", "Concats 2 Ids to a new Id (eg. concatId(a, b) == ab)", "concatId", Mode = MacroMode.MatchIdentifierOrCall)]
+    public static LNode concatId(LNode concatID, IMacroContext context)
     {
         if (concatID.ArgCount < 2)
             return null;
@@ -95,7 +95,7 @@ public static partial class BuiltInMacros
         return null;
     }
 
-    [LexicalMacro("generateId()", "Generates a new Id (eg. generateId() == a0)", Mode = MacroMode.MatchIdentifierOrCall)]
+    [LexicalMacro("generateId()", "Generates a new Id (eg. generateId() == a0)", "generateId", Mode = MacroMode.MatchIdentifierOrCall)]
     public static LNode GenerateId(LNode generateID, IMacroContext context)
     {
         var alphabet = "abcdefghijklmnopqrstuvwxyz_";
