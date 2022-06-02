7ca40275207e94a58227c7dffb57e78b54b886b7
DasLixou <einlixou@gmail.com>
2022-06-02 21:25:41 +0200

    Add Fraction Struct

---
+++ Source/Backlang.Core/Fraction.cs
@@ -0,0 +1,29 @@
+﻿namespace Backlang.Core;
+
+public struct Fraction
+{
+    private short _numerator;
+    private short _denominator;
+
+    public Fraction(Half value) : this((double)value) { }
+
+    public Fraction(float value) : this((double)value) { }
+
+    public Fraction(double value)
+    {
+        short i = 0;
+        while(value % 1 != 0)
+        {
+            value *= 10;
+            i++;
+        }
+        _numerator = (short)value;
+        _denominator = (short) (i * 10);
+    }
+
+    public Fraction(short numerator, short denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+}
---
+++ Source/TestProject1/FractionTest.cs
@@ -0,0 +1,16 @@
+﻿using Backlang.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1;
+
+[Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+public class FractionTest
+{
+    [TestMethod]
+    public void CreateFractionOfDouble()
+    {
+        var value = 1.2;
+        var fraction = new Fraction(value);
+        Assert.Equals(new Fraction(12, 10), fraction);
+    }
+}
