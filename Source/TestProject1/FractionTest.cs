a79673a172362cf8faffdf552d0d961e7177a0ea
filmee24 <filmee24@gmail.com>
2022-06-03 14:57:01 +0200

    Add Tests

--- Source/TestProject1/FractionTest.cs
+++ Source/TestProject1/FractionTest.cs
@@ -7,7 +7,7 @@ namespace TestProject1;
 public class FractionTest
 {
     [TestMethod]
-    public void CreateFractionOfDouble()
+    public void Create_Fraction_Of_Double_Should_Pass()
     {
         var value = 1.2;
         var fraction = new Fraction(value);
@@ -16,10 +16,38 @@ public class FractionTest
     }
 
     [TestMethod]
-    public void CreateNegativeFraction()
+    public void Create_Negative_Fraction_Should_Pass()
     {
         var fraction = new Fraction(-2, 3);
 
         Assert.AreEqual(new Fraction(2, 3, true), fraction);
+    }
+
+    [TestMethod]
+    public void Fraction_Division_Should_Pass()
+    {
+        var fraction = new Fraction(1, 4);
+
+        Assert.AreEqual(new Fraction(2, 4), fraction / new Fraction(1, 2));
+    }
+
+    [TestMethod]
+    public void Fraction_Muliply_Should_Pass()
+    {
+        var fraction = new Fraction(1, 2);
+
+        Assert.AreEqual(new Fraction(1, 4), fraction * fraction);
+    }
+
+    [TestMethod]
+    public void Fraction_Parse_Negative_Should_Pass()
+    {
+        Assert.AreEqual(new Fraction(2, 4, true), Fraction.Parse(@"-2 \\ 4"));
+    }
+
+    [TestMethod]
+    public void Fraction_Parse_Should_Pass()
+    {
+        Assert.AreEqual(new Fraction(2, 4), Fraction.Parse(@"2 \\ 4"));
     }
 }
