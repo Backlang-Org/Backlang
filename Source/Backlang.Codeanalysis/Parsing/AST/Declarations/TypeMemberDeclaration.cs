e5d8ec638fa58eb405027f7fb6cf2670428f2341
DasLixou <einlixou@gmail.com>
2022-06-08 11:43:28 +0200

    Change Definition of Member in Type

---
+++ Source/Backlang.Codeanalysis/Parsing/AST/Declarations/TypeMemberDeclaration.cs
@@ -0,0 +1,12 @@
+﻿using Loyc.Syntax;
+
+namespace Backlang.Codeanalysis.Parsing.AST.Declarations;
+
+public sealed class TypeMemberDeclaration
+{
+    public static LNode Parse(TokenIterator iterator, Parser parser)
+    {
+        iterator.Match(TokenType.Declare);
+        return VariableDeclaration.Parse(iterator, parser);
+    }
+}
