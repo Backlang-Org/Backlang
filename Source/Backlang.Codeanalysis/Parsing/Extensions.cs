c48565e34d8ae7ccac8a495d3c34b1553646679a
filmee24 <filmee24@gmail.com>
2022-06-09 12:21:46 +0200

    Add Extensions

---
+++ Source/Backlang.Codeanalysis/Parsing/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Loyc.Syntax;
+
+namespace Backlang.Codeanalysis.Parsing;
+
+public static class Extensions
+{
+    public static LNode WithRange(this LNode node, int start, int length)
+    {
+        return node.WithRange(new SourceRange(null, start, length));
+    }
+
+    public static LNode WithRange(this LNode node, Token token)
+    {
+        return WithRange(node, token.Start, token.Text.Length);
+    }
+
+    public static LNode WithRange(this LNode node, Token startToken, Token endToken)
+    {
+        return WithRange(node, startToken.Start, endToken.Start - startToken.Start);
+    }
+}
