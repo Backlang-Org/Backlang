c125a8529cece90923678ad68964469985dec29a
filmee24 <filmee24@gmail.com>
2022-06-10 10:46:19 +0200

    Add Annotations For Class/Struct Members

--- Source/Backlang.Codeanalysis/Parsing/AST/Declarations/StructDeclaration.cs
+++ Source/Backlang.Codeanalysis/Parsing/AST/Declarations/StructDeclaration.cs
@@ -11,7 +11,7 @@ public sealed class StructDeclaration : IParsePoint<LNode>
         var inheritances = new LNodeList();
         var members = new LNodeList();
 
-        if(iterator.Current.Type == TokenType.Colon)
+        if (iterator.Current.Type == TokenType.Colon)
         {
             do
             {
@@ -24,13 +24,15 @@ public sealed class StructDeclaration : IParsePoint<LNode>
 
         while (iterator.Current.Type != TokenType.CloseCurly)
         {
+            Annotation.TryParse(parser, out var annotations);
+
             if (iterator.Current.Type == TokenType.Function)
             {
-                members.Add(TypeFunctionDeclaration.Parse(iterator, parser));
+                members.Add(TypeFunctionDeclaration.Parse(iterator, parser).PlusAttrs(annotations));
             }
             else
             {
-                members.Add(TypeFieldDeclaration.Parse(iterator, parser));
+                members.Add(TypeFieldDeclaration.Parse(iterator, parser).PlusAttrs(annotations));
             }
         }
 
