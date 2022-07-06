e0120acb0cdbcba033bf1a3496709135ae1d6e55
filmee24 <filmee24@gmail.com>
2022-07-06 08:56:37 +0200

    Fix LiteralTest

--- Source/TestProject1/AST/Expressions/LiteralTests.cs
+++ Source/TestProject1/AST/Expressions/LiteralTests.cs
@@ -18,7 +18,7 @@ public class LiteralTests
         Assert.IsNotNull(ptrType);
         Assert.IsTrue(ptrType.Calls(Symbols.PointerType));
         Assert.IsTrue(type.Calls(Symbols.TypeLiteral));
-        Assert.AreEqual("int32", literal.Name);
+        Assert.AreEqual("int32", literal.Name.Name);
     }
 
     [TestMethod]
@@ -28,7 +28,7 @@ public class LiteralTests
         var literal = literalType.Args[0];
 
         Assert.IsNotNull(literalType);
-        Assert.AreEqual("int32", literal.Name);
+        Assert.AreEqual("int32", literal.Name.Name);
     }
 
     [TestMethod]
