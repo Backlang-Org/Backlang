07eb3b9d6654a28720ad9241ac7e6a554aef5ed2
filmee24 <filmee24@gmail.com>
2022-07-15 16:30:36 +0200

    Cleanup

--- Source/Backlang.Codeanalysis/Parsing/AST/Statements/MacroBlockStatement.cs
+++ Source/Backlang.Codeanalysis/Parsing/AST/Statements/MacroBlockStatement.cs
@@ -22,7 +22,7 @@ public sealed class MacroBlockStatement : IParsePoint<LNode>
             {
                 //custom code block with arguments
                 var body = Statement.ParseBlock(parser);
-                arguments = arguments.Add(LNode.Call(CodeSymbols.Braces, body));
+                arguments = arguments.Add(body);
 
                 return LNode.Call(nameExpression, arguments).SetStyle(NodeStyle.StatementBlock)
                     .WithRange(nameToken, iterator.Prev);
