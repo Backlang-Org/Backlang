12771534e9cbbb080fd9f83826b5b82d61a0bbfa
filmee24 <filmee24@gmail.com>
2022-07-24 13:24:40 +0200

    Cleanup

--- Source/Backlang.Driver/InternalMacros/IntrinsicsMacros.cs
+++ Source/Backlang.Driver/InternalMacros/IntrinsicsMacros.cs
@@ -12,49 +12,52 @@ public static class IntrinsicsMacros
     [LexicalMacro("inline(dotnet) {}", "Make Intrinsics Usable", "inline", Mode = MacroMode.MatchIdentifierOrCall)]
     public static LNode InlineBlock(LNode node, IMacroContext context)
     {
-        var body = node.Args.Last();
         var newBodyArgs = new LNodeList();
 
         var target = (string)context.ScopedProperties["Target"];
-        var selectedTarget = node.Args[0].Name.Name;
 
-        var compContext = (CompilerContext)context.ScopedProperties["Context"];
-
-        if (selectedTarget == LNode.Missing.Name.Name)
+        if (node is (_, var selectedTarget, var body))
         {
-            context.Warn("No target speficied for inline block. Code will never be executed");
-        }
+            var compContext = (CompilerContext)context.ScopedProperties["Context"];
 
-        if (target != selectedTarget)
-        {
-            return LNode.Call((Symbol)"'{}");
-        }
-
-        if (!compContext.CompilationTarget.HasIntrinsics)
-        {
-            context.Warn($"{selectedTarget} has no intrinsics");
-
-            return LNode.Call((Symbol)"'{}");
-        }
-
-        if (availableNames == null)
-        {
-            availableNames = InitAvailableNames(compContext.CompilationTarget.IntrinsicType);
-        }
-
-        foreach (var calls in body.Args)
-        {
-            if (!availableNames.Contains(calls.Name.Name))
+            if (selectedTarget.Name.Name == LNode.Missing.Name.Name)
             {
-                context.Error($"{calls.Name.Name} is no intrinsic");
-                continue;
+                context.Warn("No target speficied for inline block. Code will never be executed");
             }
 
-            var newCall = ConvertIntrinsic(calls, compContext.CompilationTarget.IntrinsicType);
-            newBodyArgs = newBodyArgs.Add(newCall);
+            if (target != selectedTarget.Name.Name)
+            {
+                return LNode.Call((Symbol)"'{}");
+            }
+
+            if (!compContext.CompilationTarget.HasIntrinsics)
+            {
+                context.Warn($"{selectedTarget} has no intrinsics");
+
+                return LNode.Call((Symbol)"'{}");
+            }
+
+            if (availableNames == null)
+            {
+                availableNames = InitAvailableNames(compContext.CompilationTarget.IntrinsicType);
+            }
+
+            foreach (var calls in body.Args)
+            {
+                if (!availableNames.Contains(calls.Name.Name))
+                {
+                    context.Error($"{calls.Name.Name} is no intrinsic");
+                    continue;
+                }
+
+                var newCall = ConvertIntrinsic(calls, compContext.CompilationTarget.IntrinsicType);
+                newBodyArgs = newBodyArgs.Add(newCall);
+            }
+
+            return LNode.Call((Symbol)"'{}", newBodyArgs).WithStyle(NodeStyle.Operator);
         }
 
-        return LNode.Call((Symbol)"'{}", newBodyArgs).WithStyle(NodeStyle.Operator);
+        return LNode.Missing;
     }
 
     private static LNode ConvertIntrinsic(LNode call, Type instrinsicType)
@@ -86,6 +89,6 @@ public static class IntrinsicsMacros
     {
         return intrinsicType.GetMethods()
             .Where(_ => _.IsStatic)
-            .Select(_ => _.Name.ToLower()).ToArray();
+            .Select(_ => _.Name.ToLower()).Distinct().ToArray();
     }
 }
