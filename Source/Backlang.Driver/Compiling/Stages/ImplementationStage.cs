08c5168749b9215154b2706545566284fe77ccbe
filmee24 <filmee24@gmail.com>
2022-07-29 11:50:46 +0200

    Refactor

--- Source/Backlang.Driver/Compiling/Stages/ImplementationStage.cs
+++ Source/Backlang.Driver/Compiling/Stages/ImplementationStage.cs
@@ -14,7 +14,6 @@ using Loyc;
 using Loyc.Syntax;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace Backlang.Driver.Compiling.Stages;
 
@@ -218,11 +217,6 @@ public sealed class ImplementationStage : IHandler<CompilerContext, CompilerCont
         }
 
         return block;
-    }
-
-    private static void AppendThis(BasicBlockBuilder block, IType type)
-    {
-        block.AppendInstruction(Instruction.CreateLoadArg(new Parameter(type)));
     }
 
     private static BasicBlockBuilder AppendIf(CompilerContext context, IMethod method, BasicBlockBuilder block, LNode node, QualifiedName? modulename)
@@ -510,110 +504,14 @@ public sealed class ImplementationStage : IHandler<CompilerContext, CompilerCont
         // toString method
         if (!type.Methods.Any(_ => _.Name.ToString() == "ToString" && _.Parameters.Count == 0))
         {
-            var toStringMethod = new DescribedBodyMethod(type, new SimpleName("ToString"), false, ClrTypeEnvironmentBuilder.ResolveType(context.Binder, typeof(string)));
-            toStringMethod.IsPublic = true;
-            toStringMethod.IsOverride = true;
-
-            var graph = Utils.CreateGraphBuilder();
-
-            var block = graph.EntryPoint;
-
-            var elementType = ClrTypeEnvironmentBuilder.ResolveType(context.Binder, typeof(StringBuilder));
-            var varname = Utils.GenerateIdentifier();
-            var p = block.AppendParameter(new BlockParameter(elementType, varname));
-
-            var ctor = elementType.Methods.First(_ => _.IsConstructor && _.Parameters.Count == 0);
-            var appendLineMethod = elementType.Methods.First(_ => _.Name.ToString() == "AppendLine" && _.Parameters.Count == 1 && _.Parameters[0].Type.Name.ToString() == "String");
-
-            block.AppendInstruction(Instruction.CreateNewObject(ctor, new List<ValueTag>()));
-            block.AppendInstruction(Instruction.CreateAlloca(elementType));
-
-            var loadSb = block.AppendInstruction(Instruction.CreateLoadLocal(new Parameter(p.Type, p.Tag.Name)));
-
-            AppendLine(context, block, appendLineMethod, loadSb, $"{type.FullName}:");
-
-            foreach (var field in type.Fields)
-            {
-                var loadSbf = block.AppendInstruction(Instruction.CreateLoadLocal(new Parameter(p.Type, p.Tag.Name)));
-
-                AppendLine(context, block, appendLineMethod, loadSbf, field.Name + " = ");
-                var value = AppendLoadField(block, field);
-
-                var callee = elementType.Methods.FirstOrDefault(_ => _.Name.ToString() == "Append" && _.Parameters.Count == 1 && _.Parameters[0].Type.Name.ToString() == field.FieldType.Name.ToString());
-
-                if (callee != null)
-                {
-                    block.AppendInstruction(Instruction.CreateCall(callee, MethodLookup.Virtual, new List<ValueTag> { loadSbf, value }));
-                }
-                else
-                {
-                    // call tostring on field
-                }
-            }
-
-            var tsM = elementType.Methods.First(_ => _.Name.ToString() == "ToString");
-
-            block.AppendInstruction(Instruction.CreateCall(tsM, MethodLookup.Virtual, new List<ValueTag> { loadSb }));
-
-            block.Flow = new ReturnFlow();
-
-            toStringMethod.Body = new MethodBody(new Parameter(), new Parameter(type), EmptyArray<Parameter>.Value, graph.ToImmutable());
-
-            type.AddMethod(toStringMethod);
+            Generator.GenerateToString(context, type);
         }
 
         // default constructor
         if (!type.Methods.Any(_ => _.Name.ToString() == "new" && _.Parameters.Count == type.Fields.Count))
         {
-            var ctorMethod = new DescribedBodyMethod(type, new SimpleName("new"), true, ClrTypeEnvironmentBuilder.ResolveType(context.Binder, typeof(void)))
-            {
-                IsConstructor = true
-            };
-
-            ctorMethod.IsPublic = true;
-
-            foreach (var field in type.Fields)
-            {
-                ctorMethod.AddParameter(new Parameter(field.FieldType, field.Name));
-            }
-
-            var graph = Utils.CreateGraphBuilder();
-
-            var block = graph.EntryPoint;
-
-            for (var i = 0; i < ctorMethod.Parameters.Count; i++)
-            {
-                var p = ctorMethod.Parameters[i];
-                var f = type.Fields[i];
-
-                block.AppendInstruction(Instruction.CreateLoadArg(new Parameter(type))); //this ptr
-
-                block.AppendInstruction(Instruction.CreateLoadArg(p));
-                block.AppendInstruction(Instruction.CreateStoreFieldPointer(f));
-            }
-
-            block.Flow = new ReturnFlow();
-
-            ctorMethod.Body = new MethodBody(new Parameter(), new Parameter(type), EmptyArray<Parameter>.Value, graph.ToImmutable());
-
-            type.AddMethod(ctorMethod);
+            Generator.GeneratorDefaultCtor(context, type);
         }
-    }
-
-    private static NamedInstructionBuilder AppendLoadField(BasicBlockBuilder block, IField field)
-    {
-        AppendThis(block, field.ParentType);
-        var value = block.AppendInstruction(Instruction.CreateLoadField(field));
-        return value;
-    }
-
-    private static void AppendLine(CompilerContext context, BasicBlockBuilder block, IMethod appendLineMethod, NamedInstructionBuilder argLoad, string valueStr)
-    {
-        var va = block.AppendInstruction(
-                        Instruction.CreateConstant(new StringConstant(valueStr), context.Environment.String));
-        var value = block.AppendInstruction(Instruction.CreateLoad(context.Environment.String, va));
-
-        block.AppendInstruction(Instruction.CreateCall(appendLineMethod, MethodLookup.Virtual, new List<ValueTag> { argLoad, value }));
     }
 
     private static void CollectImplementations(CompilerContext context, LNode st, QualifiedName modulename)
