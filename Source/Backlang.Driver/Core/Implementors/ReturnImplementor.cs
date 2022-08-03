678eee48d1d65e7b6f7efadf59b65fc076e4fd74
DasLixou <einlixou@gmail.com>
2022-08-03 16:27:58 +0200

    Create ReturnImplementor.cs

---
+++ Source/Backlang.Driver/Core/Implementors/ReturnImplementor.cs
@@ -0,0 +1,32 @@
+﻿using Backlang.Contracts;
+using Backlang.Contracts.Scoping;
+using Furesoft.Core.CodeDom.Compiler;
+using Furesoft.Core.CodeDom.Compiler.Core;
+using Furesoft.Core.CodeDom.Compiler.Core.Names;
+using Furesoft.Core.CodeDom.Compiler.Core.TypeSystem;
+using Furesoft.Core.CodeDom.Compiler.Flow;
+using Loyc.Syntax;
+using static Backlang.Driver.Compiling.Stages.CompilationStages.ImplementationStage;
+
+namespace Backlang.Driver.Core.Implementors;
+
+public class ReturnImplementor : IImplementor
+{
+    public BasicBlockBuilder Implement(CompilerContext context, IMethod method, BasicBlockBuilder block,
+        LNode node, QualifiedName? modulename, Scope scope)
+    {
+        if (node.ArgCount == 1)
+        {
+            var valueNode = node.Args[0];
+
+            AppendExpression(block, valueNode, (DescribedType)context.Environment.Int32, method); //ToDo: Deduce Type
+
+            block.Flow = new ReturnFlow();
+        }
+        else
+        {
+            block.Flow = new ReturnFlow();
+        }
+        return block;
+    }
+}
