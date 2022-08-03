8217855be9cba8e5a8138dc02433424efe6d92b2
DasLixou <einlixou@gmail.com>
2022-08-03 16:27:57 +0200

    Create ThrowImplementor.cs

---
+++ Source/Backlang.Driver/Core/Implementors/ThrowImplementor.cs
@@ -0,0 +1,35 @@
+﻿using Backlang.Contracts;
+using Backlang.Contracts.Scoping;
+using Furesoft.Core.CodeDom.Compiler;
+using Furesoft.Core.CodeDom.Compiler.Core;
+using Furesoft.Core.CodeDom.Compiler.Core.Names;
+using Furesoft.Core.CodeDom.Compiler.Flow;
+using Loyc.Syntax;
+using static Backlang.Driver.Compiling.Stages.CompilationStages.ImplementationStage;
+
+namespace Backlang.Driver.Core.Implementors;
+
+public class ThrowImplementor : IImplementor
+{
+    public BasicBlockBuilder Implement(CompilerContext context, IMethod method, BasicBlockBuilder block,
+        LNode node, QualifiedName? modulename, Scope scope)
+    {
+        var valueNode = node.Args[0].Args[0];
+        var constant = block.AppendInstruction(ConvertConstant(
+            GetLiteralType(valueNode, context.Binder), valueNode.Value));
+
+        var msg = block.AppendInstruction(Instruction.CreateLoad(GetLiteralType(valueNode, context.Binder), constant));
+
+        if (node.Args[0].Name.Name == "#string")
+        {
+            var exceptionType = Utils.ResolveType(context.Binder, typeof(Exception));
+            var exceptionCtor = exceptionType.Methods.FirstOrDefault(_ => _.IsConstructor && _.Parameters.Count == 1);
+
+            block.AppendInstruction(Instruction.CreateNewObject(exceptionCtor, new List<ValueTag> { msg }));
+        }
+
+        block.Flow = UnreachableFlow.Instance;
+
+        return block;
+    }
+}
