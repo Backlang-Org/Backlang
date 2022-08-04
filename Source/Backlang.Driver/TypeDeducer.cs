1a6ed9ed9f81cb244d360eb214a11b239d09f415
filmee24 <filmee24@gmail.com>
2022-08-04 09:30:39 +0200

    Add Dereference To TypeDeducer

--- Source/Backlang.Driver/TypeDeducer.cs
+++ Source/Backlang.Driver/TypeDeducer.cs
@@ -9,6 +9,7 @@ namespace Backlang.Driver;
 
 public static class TypeDeducer
 {
+    //ToDo: check for implicit cast
     public static IType Deduce(LNode node, Scope scope, CompilerContext context)
     {
         if (ImplementationStage.LiteralTypeMap.ContainsKey(node.Name))
@@ -16,7 +17,7 @@ public static class TypeDeducer
             return ImplementationStage.GetLiteralType(node, context.Binder);
         }
 
-        if (node.Calls(CodeSymbols.Add) || node.Calls(CodeSymbols.Mul)
+        if (node.Calls(CodeSymbols.Add) || node.CallsMin(CodeSymbols.Mul, 2)
             || node.Calls(CodeSymbols.Div)
             || node.Calls(CodeSymbols.Sub))
         {
@@ -29,12 +30,23 @@ public static class TypeDeducer
 
             return inner.MakePointerType(PointerKind.Transient);
         }
+        if (node.Calls(CodeSymbols.Mul))
+        {
+            var t = Deduce(node.Args[0], scope, context);
+
+            if (t is PointerType pt)
+            {
+                return pt.ElementType;
+            }
+
+            context.AddError(node, "Cannot dereference non pointer type");
+        }
 
         if (node.IsId)
         {
             if (scope.TryGet<ScopeItem>(node.Name.Name, out var item))
             {
-                return item.Type;
+                return item?.Type;
             }
             else
             {
