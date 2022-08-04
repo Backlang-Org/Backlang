90a13dfe16b42a025fc65383d894f8a3b842a27e
filmee24 <filmee24@gmail.com>
2022-08-04 11:15:33 +0200

    Refactor

--- Source/Backlang.Driver/TypeDeducer.cs
+++ Source/Backlang.Driver/TypeDeducer.cs
@@ -18,44 +18,11 @@ public static class TypeDeducer
         }
         else if (node.ArgCount == 1)
         {
-            if (node.Calls(CodeSymbols._AddressOf))
-            {
-                var inner = Deduce(node.Args[0], scope, context);
-
-                return inner.MakePointerType(PointerKind.Transient);
-            }
-            else if (node.Calls(CodeSymbols.Mul))
-            {
-                var t = Deduce(node.Args[0], scope, context);
-
-                if (t is PointerType pt)
-                {
-                    return pt.ElementType;
-                }
-
-                context.AddError(node, "Cannot dereference non pointer type");
-            }
+            return DeduceUnary(node, scope, context);
         }
         else if (node.ArgCount == 2)
         {
-            if (node.Calls(CodeSymbols.Add) || node.Calls(CodeSymbols.Mul)
-                || node.Calls(CodeSymbols.Div) || node.Calls(CodeSymbols.Sub))
-            {
-                return DeduceBinary(node, scope, context);
-            }
-            else if (node.Calls(CodeSymbols.LT)
-                || node.Calls(CodeSymbols.GT) || node.Calls(CodeSymbols.LE)
-                || node.Calls(CodeSymbols.GE))
-            {
-                NotExpectType(node[0], scope, context, context.Environment.Boolean);
-                NotExpectType(node[1], scope, context, context.Environment.Boolean);
-
-                return context.Environment.Boolean;
-            }
-            else if (node.Calls(CodeSymbols.Eq) || node.Calls(CodeSymbols.NotEq))
-            {
-                return context.Environment.Boolean;
-            }
+            return DeduceBinary(node, scope, context);
         }
         else if (node.IsId)
         {
@@ -93,6 +60,59 @@ public static class TypeDeducer
     }
 
     private static IType DeduceBinary(LNode node, Scope scope, CompilerContext context)
+    {
+        if (node.Calls(CodeSymbols.Add) || node.Calls(CodeSymbols.Mul)
+                || node.Calls(CodeSymbols.Div) || node.Calls(CodeSymbols.Sub))
+        {
+            return DeduceBinaryHelper(node, scope, context);
+        }
+        else if (node.Calls(CodeSymbols.LT)
+            || node.Calls(CodeSymbols.GT) || node.Calls(CodeSymbols.LE)
+            || node.Calls(CodeSymbols.GE))
+        {
+            NotExpectType(node[0], scope, context, context.Environment.Boolean);
+            NotExpectType(node[1], scope, context, context.Environment.Boolean);
+
+            return context.Environment.Boolean;
+        }
+        else if (node.Calls(CodeSymbols.Eq) || node.Calls(CodeSymbols.NotEq))
+        {
+            return context.Environment.Boolean;
+        }
+
+        return null;
+    }
+
+    private static IType DeduceUnary(LNode node, Scope scope, CompilerContext context)
+    {
+        if (node.Calls(CodeSymbols._AddressOf))
+        {
+            var inner = Deduce(node.Args[0], scope, context);
+
+            return inner.MakePointerType(PointerKind.Transient);
+        }
+        else if (node.Calls(CodeSymbols.Mul))
+        {
+            var t = Deduce(node.Args[0], scope, context);
+
+            if (t is PointerType pt)
+            {
+                return pt.ElementType;
+            }
+
+            context.AddError(node, "Cannot dereference non pointer type");
+        }
+        else if (node.Calls(CodeSymbols.Not))
+        {
+            ExpectType(node.Args[0], scope, context, context.Environment.Boolean);
+
+            return context.Environment.Boolean;
+        }
+
+        return null;
+    }
+
+    private static IType DeduceBinaryHelper(LNode node, Scope scope, CompilerContext context)
     {
         var left = Deduce(node.Args[0], scope, context);
         var right = Deduce(node.Args[1], scope, context);
