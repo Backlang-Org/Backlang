11621776b7314e7fedcf6ae0ffd9a6fd9591e6f3
filmee24 <filmee24@gmail.com>
2022-08-06 16:49:55 +0200

    Fix EmbeddedResources

---
+++ Source/Backlang.Driver/Compiling/Stages/InitStages/InitEmbeddedResourcesStage.cs
@@ -0,0 +1,27 @@
+﻿using Flo;
+
+namespace Backlang.Driver.Compiling.Stages.InitStages;
+
+public sealed class InitEmbeddedResourcesStage : IHandler<CompilerContext, CompilerContext>
+{
+    public async Task<CompilerContext> HandleAsync(CompilerContext context, Func<CompilerContext, Task<CompilerContext>> next)
+    {
+        foreach (var resource in context.EmbeddedResource)
+        {
+            Stream strm = File.OpenRead(resource);
+
+            foreach (var preprocessor in context.Plugins?.Preprocessors)
+            {
+                if (preprocessor.Extension == Path.GetExtension(resource))
+                {
+                    strm = preprocessor.Preprocess(strm);
+                }
+            }
+
+            var attr = new EmbeddedResourceAttribute(Path.GetFileName(resource), strm);
+            context.Assembly.AddAttribute(attr);
+        }
+
+        return await next.Invoke(context);
+    }
+}
