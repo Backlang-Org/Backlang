aa3ca2d7472c0fc8d4b85d537599c0139c413421
filmee24 <filmee24@gmail.com>
2022-08-08 18:42:43 +0200

    Add AssignmentImplentor For Variables

---
+++ Source/Backlang.Driver/Core/Implementors/Statements/AssignmentImplementor.cs
@@ -0,0 +1,35 @@
+﻿using Backlang.Contracts.Scoping.Items;
+using static Backlang.Driver.Compiling.Stages.CompilationStages.ImplementationStage;
+
+namespace Backlang.Driver.Core.Implementors.Statements;
+
+public class AssignmentImplementor : IStatementImplementor
+{
+    public BasicBlockBuilder Implement(CompilerContext context, IMethod method, BasicBlockBuilder block, LNode node, QualifiedName? modulename, Scope scope)
+    {
+        if (node is (_, var left, var right))
+        {
+            var lt = TypeDeducer.Deduce(left, scope, context);
+            var rt = TypeDeducer.Deduce(right, scope, context);
+            TypeDeducer.ExpectType(right, scope, context, lt);
+
+            if (scope.TryGet<VariableScopeItem>(left.Name.Name, out var va))
+            {
+                if (!va.IsMutable)
+                {
+                    context.AddError(node, $"Cannot assing immutable variable '{va.Name}'");
+                }
+
+                var value = AppendExpression(block, right, rt, context, scope);
+
+                block.AppendInstruction(Instruction.CreateStore(lt, new ValueTag(va.Parameter.Name.ToString()), value));
+            }
+            else
+            {
+                context.AddError(node, $"Variable '{left.Name.Name}' not found");
+            }
+        }
+
+        return block;
+    }
+}
