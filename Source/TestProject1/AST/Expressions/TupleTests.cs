d25085facd0220f8907a26ac60ac90611b9a5226
filmee24 <filmee24@gmail.com>
2022-08-24 17:27:57 +0200

    Add TupleTests

---
+++ Source/TestProject1/AST/Expressions/TupleTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.AST.Expressions;
+
+[TestClass]
+public class TupleTests : ParserTestBase
+{
+    [TestMethod]
+    public void SimpleTuple_Should_Pass()
+    {
+        var src = "(1, 2);";
+        var tree = ParseAndGetNodesInFunction(src);
+    }
+
+    [TestMethod]
+    public void TrailingComma_Should_Fail()
+    {
+        var src = "(1,2,);";
+        var tree = ParseAndGetNodesInFunction(src);
+    }
+}
