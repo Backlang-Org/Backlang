24f0ad418253430c04757cafa4deedc99d23258f
filmee24 <filmee24@gmail.com>
2022-08-26 12:42:21 +0200

    Add Core For Wasm Backend

---
+++ Source/Plugins/Backlang.Backends.Wasm/WasmTarget.cs
@@ -0,0 +1,44 @@
+﻿using Backlang.Contracts;
+using Furesoft.Core.CodeDom.Compiler.Core.TypeSystem;
+using Furesoft.Core.CodeDom.Compiler.Pipeline;
+using LeMP;
+
+namespace Backlang.Backends.Wasm
+{
+    public class WasmTarget : ICompilationTarget
+    {
+        public Type IntrinsicType => null;
+
+        public string Name => "wasm";
+
+        public void AfterCompiling(CompilerContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeforeCompiling(CompilerContext context)
+        {
+            context.OutputFilename += ".wasm";
+        }
+
+        public void BeforeExpandMacros(MacroProcessor processor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITargetAssembly Compile(AssemblyContentDescription contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeEnvironment Init(CompilerContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitReferences(CompilerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
