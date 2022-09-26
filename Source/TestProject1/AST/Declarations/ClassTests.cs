4c88165033d47416233e5ed5f296a25548a0bd09
filmee24 <filmee24@gmail.com>
2022-09-26 20:36:31 +0200

    Add ClassTests

---
+++ Source/TestProject1/AST/Declarations/ClassTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.AST.Declarations;
+
+[TestClass]
+public class ClassTests : ParserTestBase
+{
+    [TestMethod]
+    public void Simple_Class_Should_Pass()
+    {
+        var src = "class Point { let X : i32; let Y : i32; }";
+        var result = ParseAndGetNodes(src);
+
+        Assert.AreEqual(0, result.errors.Count);
+    }
+
+    [TestMethod]
+    public void Class_With_Inheritance_Should_Pass()
+    {
+        var src = "class Point : IHello, IWorld { let X : i32; let Y : i32; }";
+        var result = ParseAndGetNodes(src);
+
+        Assert.AreEqual(0, result.errors.Count);
+    }
+}
