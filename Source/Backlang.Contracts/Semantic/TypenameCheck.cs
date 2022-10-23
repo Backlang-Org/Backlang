50c828f2244f1c8698ba677beb5339cb9cbce548
filmee24 <filmee24@gmail.com>
2022-10-23 16:46:52 +0200

    Add New Semantic Checks

---
+++ Source/Backlang.Contracts/Semantic/TypenameCheck.cs
@@ -0,0 +1,23 @@
+﻿using Backlang.Codeanalysis.Core;
+using Backlang.Driver;
+
+namespace Backlang.Contracts.Semantic;
+
+internal class TypenameCheck : ISemanticCheck
+{
+    public void Check(CompilationUnit tree, CompilerContext context)
+    {
+        for (var i = 0; i < tree.Body.Count; i++)
+        {
+            var node = tree.Body[i];
+
+            if (node.Calls(CodeSymbols.Class) || node.Calls(CodeSymbols.Struct))
+            {
+                if (node is (_, var typename, _) && char.IsLower(typename.Name.Name[0]))
+                {
+                    context.Messages.Add(Message.Warning($"Type '{typename.Name.Name}' should be Uppercase", node.Range));
+                }
+            }
+        }
+    }
+}
