25e24139802558d1ce7271c884b68d3ea2aa8bf6
filmee24 <filmee24@gmail.com>
2022-10-31 08:03:13 +0100

    Move ErrorCode Enum In Seperate File

---
+++ Source/Backlang.Codeanalysis/Core/ErrorID.cs
@@ -0,0 +1,22 @@
+﻿namespace Backlang.Codeanalysis.Core;
+
+public enum ErrorID : int
+{
+    UnknownCharacter = 3,
+    UnterminatedCharLiteral = 4,
+    UnknownExpression = 5,
+    UnknownLiteral = 6,
+    ForbiddenTrailingComma = 7,
+    BitfieldNotLiteral = 8,
+    UnexpecedTypeMember = 9,
+    ExpectedTypeLiteral = 10,
+    UnknownSwitchOption = 11,
+    NoCatchBlock = 12,
+    EmptyFile = 13,
+    ExpectedIdentifier = 14,
+    UnterminatedLiteral = 15,
+    NotClosedMultilineComment = 16,
+    Expected = 17,
+    DuplicateModifier = 18,
+    UnexpecedType = 19
+}
--- Source/Backlang.Codeanalysis/Core/LocalizableString.cs
+++ Source/Backlang.Codeanalysis/Core/LocalizableString.cs
@@ -50,24 +50,3 @@ public readonly struct LocalizableString
         return new(message);
     }
 }
-
-public enum ErrorID : int
-{
-    UnknownCharacter = 3,
-    UnterminatedCharLiteral = 4,
-    UnknownExpression = 5,
-    UnknownLiteral = 6,
-    ForbiddenTrailingComma = 7,
-    BitfieldNotLiteral = 8,
-    UnexpecedTypeMember = 9,
-    ExpectedTypeLiteral = 10,
-    UnknownSwitchOption = 11,
-    NoCatchBlock = 12,
-    EmptyFile = 13,
-    ExpectedIdentifier = 14,
-    UnterminatedLiteral = 15,
-    NotClosedMultilineComment = 16,
-    Expected = 17,
-    DuplicateModifier = 18,
-    UnexpecedType = 19
-}
