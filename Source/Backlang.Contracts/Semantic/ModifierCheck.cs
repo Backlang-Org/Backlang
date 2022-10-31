cd4250bcce9c4fc868a96e5a0c941a63372ebdf9
filmee24 <filmee24@gmail.com>
2022-10-31 07:43:04 +0100

    Add Modifier Semantic Check

---
+++ Source/Backlang.Contracts/Semantic/ModifierCheck.cs
@@ -0,0 +1,35 @@
+﻿using Backlang.Codeanalysis.Core;
+
+namespace Backlang.Contracts.Semantic;
+
+internal class ModifierCheck : ISemanticCheck
+{
+    public void Check(CompilationUnit tree, CompilerContext context)
+    {
+        var nodesWithModifiers = tree.Body
+            .SelectMany(_ => _.DescendantsAndSelf()).Where(IsModifiableNode).ToArray();
+
+        foreach (var node in nodesWithModifiers)
+        {
+            CheckForInvalidModifierCombination(node, context);
+        }
+    }
+
+    private void CheckForInvalidModifierCombination(LNode node, CompilerContext context)
+    {
+        var attrs = node.Attrs;
+        var condition = attrs.Contains(LNode.Id(CodeSymbols.Public)) && attrs.Contains(LNode.Id(CodeSymbols.Private))
+             || attrs.Contains(LNode.Id(CodeSymbols.Public)) && attrs.Contains(LNode.Id(CodeSymbols.Internal))
+             || attrs.Contains(LNode.Id(CodeSymbols.Private)) && attrs.Contains(LNode.Id(CodeSymbols.Internal));
+
+        if (condition)
+        {
+            context.AddError(node, "Invalid Modifier Combination");
+        }
+    }
+
+    private bool IsModifiableNode(LNode arg)
+    {
+        return arg.Calls(CodeSymbols.Class) || arg.Calls(CodeSymbols.Struct) || arg.Calls(CodeSymbols.Fn);
+    }
+}
