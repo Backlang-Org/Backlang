c8ad4a92d890e6021fa8697b8edf886f0ef91a3b
filmee24 <filmee24@gmail.com>
2022-11-02 11:09:17 +0100

    Refactor ArithmetikEmitter

--- Source/Backlang.Driver/Compiling/Targets/Dotnet/Emitters/ArithmetikEmitter.cs
+++ Source/Backlang.Driver/Compiling/Targets/Dotnet/Emitters/ArithmetikEmitter.cs
@@ -6,46 +6,48 @@ namespace Backlang.Driver.Compiling.Targets.Dotnet.Emitters;
 
 internal class ArithmetikEmitter : IEmitter
 {
+    private readonly Dictionary<string, OpCode> _stringOPMap = new()
+    {
+        ["arith.+"] = OpCodes.Add,
+        ["arith.-"] = OpCodes.Sub,
+        ["arith.*"] = OpCodes.Mul,
+        ["arith./"] = OpCodes.Div,
+
+        ["arith.%"] = OpCodes.Div,
+
+        ["arith.&"] = OpCodes.And,
+        ["arith.|"] = OpCodes.Or,
+        ["arith.^"] = OpCodes.Xor,
+
+        ["arith.<"] = OpCodes.Clt,
+        ["arith.>"] = OpCodes.Cgt,
+        ["arith.=="] = OpCodes.Ceq,
+    };
+
     public void Emit(AssemblyDefinition assemblyDefinition, ILProcessor ilProcessor, Furesoft.Core.CodeDom.Compiler.Instruction instruction, BasicBlock block)
     {
         var arithProtype = (IntrinsicPrototype)instruction.Prototype;
 
+        if (_stringOPMap.ContainsKey(arithProtype.Name))
+        {
+            var op = _stringOPMap[arithProtype.Name];
+            ilProcessor.Emit(op); return;
+        }
+
         switch (arithProtype.Name)
         {
-            case "arith.+":
-                ilProcessor.Emit(OpCodes.Add); break;
-            case "arith.-":
-                ilProcessor.Emit(OpCodes.Sub); break;
-            case "arith.*":
-                ilProcessor.Emit(OpCodes.Mul); break;
-            case "arith./":
-                ilProcessor.Emit(OpCodes.Div); break;
-            case "arith.%":
-                ilProcessor.Emit(OpCodes.Rem); break;
-            case "arith.&":
-                ilProcessor.Emit(OpCodes.And); break;
-            case "arith.|":
-                ilProcessor.Emit(OpCodes.Or); break;
-            case "arith.^":
-                ilProcessor.Emit(OpCodes.Xor); break;
-            case "arith.==":
-                ilProcessor.Emit(OpCodes.Ceq); break;
             case "arith.!=":
                 ilProcessor.Emit(OpCodes.Ceq);
                 ilProcessor.Emit(OpCodes.Ldc_I4, 0);
                 ilProcessor.Emit(OpCodes.Ceq);
                 break;
 
-            case "arith.<":
-                ilProcessor.Emit(OpCodes.Clt); break;
             case "arith.<=":
                 ilProcessor.Emit(OpCodes.Cgt);
                 ilProcessor.Emit(OpCodes.Ldc_I4, 0);
                 ilProcessor.Emit(OpCodes.Ceq);
                 break;
 
-            case "arith.>":
-                ilProcessor.Emit(OpCodes.Cgt); break;
             case "arith.>=":
                 ilProcessor.Emit(OpCodes.Clt);
                 ilProcessor.Emit(OpCodes.Ldc_I4, 0);
