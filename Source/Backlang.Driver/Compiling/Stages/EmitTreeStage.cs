22c8d600f8dbdce5524cf47b02ac438fece9fd91
filmee24 <filmee24@gmail.com>
2022-11-13 11:00:28 +0100

    Cleanup

--- Source/Backlang.Driver/Compiling/Stages/EmitTreeStage.cs
+++ Source/Backlang.Driver/Compiling/Stages/EmitTreeStage.cs
@@ -6,23 +6,20 @@ public sealed class EmitTreeStage : IHandler<CompilerContext, CompilerContext>
 {
     public async Task<CompilerContext> HandleAsync(CompilerContext context, Func<CompilerContext, Task<CompilerContext>> next)
     {
-        if (!context.Messages.Any())
+        var sb = new StringBuilder();
+        var tree = context.Trees.FirstOrDefault();
+
+        if (tree == null)
         {
-            var sb = new StringBuilder();
-            var tree = context.Trees.FirstOrDefault();
-
-            if (tree == null)
-            {
-                return context;
-            }
-
-            foreach (var node in tree.Body)
-            {
-                sb.AppendLine(node.ToString());
-            }
-
-            File.WriteAllText(Path.Combine(context.TempOutputPath, context.Options.OutputFilename + ".txt"), sb.ToString());
+            return context;
         }
+
+        foreach (var node in tree.Body)
+        {
+            sb.AppendLine(node.ToString());
+        }
+
+        File.WriteAllText(Path.Combine(context.TempOutputPath, context.Options.OutputFilename + ".txt"), sb.ToString());
 
         return await next.Invoke(context);
     }
