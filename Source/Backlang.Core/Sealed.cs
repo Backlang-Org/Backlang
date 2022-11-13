79a99acb51c2742ed521a3a337b80c7d777830ac
filmee24 <filmee24@gmail.com>
2022-11-13 17:54:55 +0100

    Add Sealed Container

---
+++ Source/Backlang.Core/Sealed.cs
@@ -0,0 +1,32 @@
+﻿namespace Backlang.Core;
+
+public struct Sealed<T>
+{
+    private T _value;
+
+    public bool IsFreezed { get; private set; }
+
+    public static implicit operator Sealed<T>(T value)
+    {
+        return new Sealed<T> { _value = value, IsFreezed = true };
+    }
+
+    public static implicit operator T(Sealed<T> @sealed)
+    {
+        return @sealed._value;
+    }
+
+    public void Set(T value)
+    {
+        if (IsFreezed)
+        {
+            throw new InvalidOperationException("Object is freezed");
+        }
+        _value = value;
+    }
+
+    public void Unfreeze()
+    {
+        IsFreezed = true;
+    }
+}
