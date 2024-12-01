13ba5175be2c814d63fe0619d05059bad2669e81
Chris Anders <filmee24@gmail.com>
2024-12-01 19:41:51 +0100

    refactor: Cleanup

---
+++ NewSource/BacklangC/Stages/SaveModuleStage.cs
@@ -0,0 +1,13 @@
+﻿using Flo;
+
+namespace BacklangC.Stages;
+
+public sealed class SaveModuleStage : IHandler<Driver, Driver>
+{
+    public async Task<Driver> HandleAsync(Driver context, Func<Driver, Task<Driver>> next)
+    {
+        context.Compilation.Module.Save(context.Settings.OutputPath, context.Settings.DebugSymbols);
+
+        return await next.Invoke(context);
+    }
+}
